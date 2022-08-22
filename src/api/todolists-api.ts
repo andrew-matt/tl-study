2666a51d69b06ceaaf7d1edbdf5b2ca4b3e992c5
andrew-matt <andrew.matt@ya.ru>
2022-08-22 14:53:17 +0300

    update

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,8 +4,8 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': 'ba20e791-30f7-4ca9-a0a3-61abf10b8541',
-    }
+        'API-KEY': process.env.REACT_APP_API_KEY as string,
+    },
 });
 
 // api
